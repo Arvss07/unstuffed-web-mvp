b3b602589ebaa01d38f94dfcf46f2ceb67b34cde
ARVY AGGABAO <2233162@slu.edu.ph>
2024-11-18 19:57:35 +0800

    added shadcnUI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: "8080",
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "lib",
+        replacement: resolve(__dirname, "lib"),
+      },
+    ],
+  },
+});
