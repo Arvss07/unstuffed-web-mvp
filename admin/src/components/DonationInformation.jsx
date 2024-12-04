2e1f70ddbb3b8b960604c27b8132c35343765b5b
Judrey Padsuyan <2232249@slu.edu.ph>
2024-12-04 23:34:14 +0800

    Created initial changes in the Donation Information

--- admin/src/components/DonationInformation.jsx
+++ admin/src/components/DonationInformation.jsx
@@ -5,7 +5,7 @@ const DonationInformation = ({ setClose, donation }) => {
   const photoUrls = donation.items_donated_link.split(",");
 
   return (
-    <div className="fixed w-full h-screen bg-primary_grey top-0 left-0 right-0 bg-opacity-50 flex items-center justify-center p-10">
+    <div className="fixed w-full h-screen bg-blue_bg top-0 left-0 right-0 bg-opacity-50 flex items-center justify-center p-10">
       <div className="bg-white h-full w-full rounded-xl p-8 overflow-y-auto">
         <div className="flex justify-between mb-6">
           <h1 className="font-bold text-2xl">Donation's Information</h1>
@@ -17,7 +17,7 @@ const DonationInformation = ({ setClose, donation }) => {
           </button>
         </div>
 
-        <div className="grid grid-cols-2 gap-80">
+        <div className="grid grid-cols-2 gap-20 pl-10">
           <div className="space-y-4">
             <div>
               <label className="block text-sm font-medium text-blue_text mb-1">
@@ -26,7 +26,7 @@ const DonationInformation = ({ setClose, donation }) => {
               <Input 
                 value={`${donation.firstName} ${donation.lastName}`}
                 disabled
-                className="w-full bg-drop_bg rounded-full shadow-xl"
+                className="w-full text-sm bg-drop_bg rounded-full shadow-xl"
               />
             </div>
 
@@ -37,7 +37,7 @@ const DonationInformation = ({ setClose, donation }) => {
               <Input 
                 value={donation.email}
                 disabled
-                className="w-full bg-drop_bg rounded-full shadow-xl"
+                className="w-full text-sm bg-drop_bg rounded-full shadow-xl"
               />
             </div>
 
@@ -48,7 +48,7 @@ const DonationInformation = ({ setClose, donation }) => {
               <Input 
                 value={donation.user_location}
                 disabled
-                className="w-full bg-drop_bg rounded-full shadow-xl"
+                className="w-full text-sm bg-drop_bg rounded-full shadow-xl"
               />
             </div>
 
@@ -59,7 +59,7 @@ const DonationInformation = ({ setClose, donation }) => {
               <Input 
                 value={donation.items_donated}
                 disabled
-                className="w-full bg-drop_bg rounded-full shadow-xl"
+                className="w-full text-sm bg-drop_bg rounded-full shadow-xl"
               />
             </div>
 
@@ -70,7 +70,7 @@ const DonationInformation = ({ setClose, donation }) => {
               <Input 
                 value={donation.ngo.location}
                 disabled
-                className="w-full bg-drop_bg rounded-full shadow-xl"
+                className="w-full text-sm bg-drop_bg rounded-full shadow-xl"
               />
             </div>
           </div>
--- admin/src/components/Input.jsx
+++ admin/src/components/Input.jsx
@@ -4,7 +4,7 @@ export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <input
       ref={ref}
-      className={`w-[1000px] px-3 py-3 border-black rounded-md disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+      className={`w-[1000px] px-2 py-3 border-black rounded-md disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       {...props}
     />
   );
